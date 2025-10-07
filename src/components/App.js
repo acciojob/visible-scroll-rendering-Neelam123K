6e014284038105d706f54460361c5150c0712bba
Neelam123K <neelamk31032002@gmail.com>
2025-10-07 18:08:58 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
-import React from "react";
-import VisibleScroll from "./VisibleScroll";
-import './../styles/App.css';
+import React, { useRef } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-        <VisibleScroll />
-    </div>
-  )
-}
-
-export default App
+  const arr = useRef(Array.from({length:150},(_,i)=>(i+1)));
+  return <div style={{height:"500px",overflowX:"hidden",overflowY:"scroll",scrollbarWidth:"none",textAlign:"center"}}>
+    {arr.current.map((v,i)=><Item n={i}  key={i+v}/>)}
+  </div>;
+};``
+const Item = ({n}) => (
+  <>
+    <h1>Item {n} </h1>
+    <p>Lorem ipsum dolor sit amet</p>
+  </>
+);
+export default App;
